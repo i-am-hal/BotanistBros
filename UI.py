621e6f69be544310eb3d75e872953c968ab2962f
i-am-hal <alastar.quartz@gmail.com>
2023-04-16 16:31:28 -0800

    This is strange. Refactored code, still displays to LCD, but now I can test the program in a way more analogous to when button inputs are introduced. (The program doesn't, say, stop execution to ensure one of the buttons is pressed.) This not only makes MY life easier writing this program, but for anyone in the future that decides to test this software.

--- UI.py
+++ UI.py
@@ -6,10 +6,18 @@ Info:   A series of routines, definitions, and classes
 which enable the user interface for the Plant Nanny.
 UI is displayed on a LCD screen, 4x20 characters.
 """
+import curses
+from curses import wrapper
 from math import ceil
-from time import sleep
+from serial import Serial
 from enum import Enum, auto
+from time import sleep
+from inputs import readMoisture
+from datetime import datetime, timedelta
 from liquidcrystal_i2c import LiquidCrystal_I2C as I2C_LCD
+
+#Globals for location and port for the serial
+SER_LOC, SER_PORT = "/dev/ttyACM0", 9600
 
 #This is a class for the convenience of me, Alastar. For various
 # reasons the actual physical LCD screen I have is being almost
@@ -17,14 +25,24 @@ from liquidcrystal_i2c import LiquidCrystal_I2C as I2C_LCD
 # and have it update the physical screen as I would hope, while
 # allowing me to see and test the UI on an actual computer screen.
 class LCD:
-	def __init__(self, addr, bus, numlines=2, linelen=20):
+	def __init__(self, addr, bus, curseScrn, numlines=2, linelen=20):
 		self.lcd = I2C_LCD(addr, bus, numlines=numlines) 
+		self.curse = curseScrn
 		self.screen = [" " * linelen] * numlines
 		self.linelength = linelen
 		self.numlines   = numlines
 	
+	def getch(self) -> chr:
+		char = self.curse.getch()
+		
+		#Do not accept any non-ascii char
+		if char < 32:
+			return ' '
+		
+		return chr(char)
+	
 	def eraseScreen(self):
-		print("\n" * 100)	
+		self.curse.erase()
 
 	def writeLine(self, line, string):
 		#raise error if too large a value
@@ -43,21 +61,17 @@ class LCD:
 		self.screen[line] = "".join(scrnLine)
 	
 	#Print out all te lines in the virtual screen
-	def updateScreen(self):
-		print('\n'.join(self.screen))
+	def refresh(self):
+		self.eraseScreen()	
+		for (string, line) in zip(self.screen, range(self.numlines)):
+			self.curse.addstr(line, 0, string)	
+		self.curse.refresh()
 	
 	def printline(self, line, string):
-		self.eraseScreen()
 		self.writeLine(line, string)
 		self.lcd.printline(line, string)
-		self.updateScreen()
 		
 
-#Instantiates the lcd screen that we will be using this whole time
-#screen = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
-screen = LCD(0x27, 1, numlines=4)
-screen.eraseScreen()
-#screen.backlight() #255)
 
 #Time unit, can be day or week
 class Time(Enum):
@@ -71,6 +85,7 @@ class DelayOption:
 	def __init__(self, unit, amount):
 		self.unit   = unit
 		self.amount = amount
+		self.deltatime = self.toTimeDelta()
 	
 	def __repr__(self):
 		suffix = {Time.DAY:'D', Time.WEEK:'W'}
@@ -82,6 +97,17 @@ class DelayOption:
 
 	def __len__(self):
 		return len(self.__str__())		  
+	
+	#Takes this delay option, and return it as a timedelta object
+	def toTimeDelta(self):
+		if self.unit == Time.DAY:
+			return timedelta(days=self.amount)	
+
+		elif self.unit == Time.WEEK:
+			return timedelta(weeks=self.amount)
+		
+		#elif self.unit == Time.MONTH:
+		#	return timedelta(days=self.amount * 30)
 
 class WaterOption:
 	#Percent is just an integer 1-100
@@ -141,15 +167,13 @@ tabSelLines = {
 	1: "-" * 6 + " " * 9 + 5 * "-"
 }
 
-#The index (which tab) is selected
-tab_select = 0
 #Select tab name rom index
 selectedTab = lambda i: list(UI_tabs)[i]
 
 #Takes tab index, wraps to first tab if going past options, else, next option
 wrapTab = lambda n: 0 if n >= len(list(UI_tabs)) else n
 
-def update_ui():
+def update_ui(screen, tab_select):
 	#Using the current selection, pick relevent line below tab names
 	screen.printline(1, tabSelLines[tab_select])
 
@@ -162,36 +186,44 @@ def update_ui():
 	screen.printline(2, center(str(options[opt_sel])))
 	#Display to user that they have N/M selected in tab
 	screen.printline(3, " " * 15 + f"{opt_sel+1:02d}/{max_opt:02d}")
+	#screen.updateScreen()
 
 #
-def display_ui():
+def display_ui(screen, tab_select):
 	#Display the tab names at the very top
 	screen.printline(0, " | ".join(UI_tabs) + " |")
-	update_ui()
+	update_ui(screen, tab_select)
 
 #Ensures that the OPT_LEN properties are the required length
 def prep_opt_length():
 	for tab in UI_tabs:
 		UI_tabs[tab][OPT_LEN] = len(UI_tabs[tab][OPTIONS])
 
+def main(curseScrn):
+	curseScrn.nodelay(True)
+	#Instantiates the lcd screen that we will be using this whole time
+	#screen = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
+	screen = LCD(0x27, 1, curseScrn, numlines=4)
+	#screen.eraseScreen()
+	#screen.backlight() #255)
 
-if __name__ == "__main__":
 	prep_opt_length()
+
+	#The index (which tab) is selected
+	tab_select = 0
 	
-	display_ui()
-	
+	display_ui(screen, tab_select)
+
 	cmd = ""	
 
-	print("Enter 1 or s to change tab.\nEnter 2 or o to change option.\n\n")
+	#print("Enter 1 or s to change tab.\nEnter 2 or o to change option.\n\n")
 
 	while True:
-		cmd = input()[:1]
-		
 		#Go to next tab
 		if cmd in "1s":
 			tab_select = wrapTab(tab_select+1)
-			print(f"Next tab {tab_select}")
-			update_ui()
+			#print(f"Next tab {tab_select}")
+			update_ui(screen, tab_select)
 		
 		#Cycle through options in this tab
 		elif cmd in "2o":
@@ -205,5 +237,12 @@ if __name__ == "__main__":
 				UI_tabs[tab][SELECTION] += 1
 
 			#Update no matter what
-			update_ui()
+			update_ui(screen, tab_select)
 
+		#display_ui(screen, tab_select)
+		screen.refresh()
+		cmd = screen.getch()
+
+
+if __name__ == "__main__":
+	wrapper(main)
